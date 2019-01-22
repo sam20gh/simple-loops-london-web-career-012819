3bdc6bea79af390c7a875c540980629dfa7d658c
sam ghoreishi <sam20gh@gmail.com>
2019-01-22 09:14:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -29,10 +29,8 @@ until counter == 7
 end
 
 def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+ for counter in 1..40 do
+  puts "The current number is #{counter}."
   
 end
 
